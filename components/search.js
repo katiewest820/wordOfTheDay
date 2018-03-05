f0529054ad26333e36a8338f05e569540fa8a634
Katie West <katiewest820@gmail.com>
2018-03-04 16:15:45 -0800

    api call added

--- components/search.js
+++ components/search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default class Search extends React.Component{
@@ -11,7 +12,15 @@ export default class Search extends React.Component{
   }
 
   callAPIForWord(){
-    console.log(this.state.searchEntry)
+    let word = this.state.searchEntry;
+    let key = '1545b079e732c69e8bcd3982db07818a';
+    let id = '9e7d5728';
+    axios.get(`https://od-api.oxforddictionaries.com:443/api/v1/entries/en/${word}`, { headers: { app_id: id, app_key: key } })
+    .then((response) => {
+      console.log(response)
+    }).catch((err) => {
+      console.log(err)
+    })
   }
 
 
